551eb8b3013fc2862f3bb8767c1b02ca8b7ddc7e
kp41612 <piotrek.korzen1@gmail.com>
2019-12-18 13:47:06 +0100

    2 files modified

--- src/app/lista-wydatkow.service.ts
+++ src/app/lista-wydatkow.service.ts
@@ -12,7 +12,7 @@ export class WydatkiService {
       new Wydatek(1, 'Tankowanie', 252.43, '', new Date(2018, 4, 30), 527.5, 48),
       new Wydatek(2, 'Myjnia', 75, '', new Date(2018, 4, 2)),
       new Wydatek(3, 'Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16)),
-      new Wydatek(4, 'Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16)),
+      new Wydatek(4, 'Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16),null,25),
       new Wydatek(5, 'Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12), 527),
       new Wydatek(6, 'Tankowanie', 257.30, '', new Date(2017, 7, 13), null, 35)
     ]; 
